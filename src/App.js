2102df6f9cd7d3dafea63d32d8185d554da5f208
oreoluwade <oreoluwade@gmail.com>
2019-11-21 10:11:39 +0000

    Make app usage more descriptive

--- src/App.js
+++ src/App.js
@@ -6,11 +6,28 @@ const App = () => {
         <div className="root">
             <div className="app-description">
                 <p>
-                    Type a number into the input box, smash the enter key and
-                    see your number added to the list
+                    The Application has a text input that only accepts integers
                 </p>
                 <p>
-                    Click on the checkbox next to a number to cross the number
+                    When you press enter on the input, the number will be added
+                    to the list
+                </p>
+                <p>
+                    The list of numbers will always show the highest number
+                    first, the lowest number last, and the number that would be
+                    in the middle of the list if sorted in the middle
+                </p>
+                <p>
+                    The rest of the numbers appear in the same order they are
+                    added
+                </p>
+                <p>
+                    When a number's checkbox is checked, the number will have a
+                    line through as indication of an excluded number
+                </p>
+                <p>
+                    If checked, the number cannot be considered as maximum,
+                    minimum or middle
                 </p>
                 <p>Reset the app to the default state with the reset button</p>
             </div>
