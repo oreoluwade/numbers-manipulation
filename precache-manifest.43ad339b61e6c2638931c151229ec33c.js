486d86465f30ba33f39c455c23d39fde09f9511f
oreoluwade <oreoluwade@gmail.com>
2019-11-21 11:04:23 +0000

    Updates

---
+++ precache-manifest.43ad339b61e6c2638931c151229ec33c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "994668e75e49af751acba3f9ab759c57",
+    "url": "/numbers-manipulation/index.html"
+  },
+  {
+    "revision": "4c8eaa8f5f0578d17c24",
+    "url": "/numbers-manipulation/static/css/main.fb68d6d1.chunk.css"
+  },
+  {
+    "revision": "46cbe073cf5f7657c5a2",
+    "url": "/numbers-manipulation/static/js/2.5b1e9781.chunk.js"
+  },
+  {
+    "revision": "4c8eaa8f5f0578d17c24",
+    "url": "/numbers-manipulation/static/js/main.f078fa5c.chunk.js"
+  },
+  {
+    "revision": "91dd951b0ad8bb26b328",
+    "url": "/numbers-manipulation/static/js/runtime-main.b4e75c40.js"
+  }
+]);
